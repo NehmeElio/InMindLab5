c6cdaf2c3ec0de3327565bb9e53207bbc5b4cbed
Elio <elion628@gmail.com>
2024-07-22 04:55:01 +0300

    All feature of database commited

---
+++ UMS_Lab5.Domain/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UMS_Lab5.Persistence.UMS_Lab5.Domain.Models;
+
+public partial class User
+{
+    public long Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public long RoleId { get; set; }
+
+    public string FirebaseId { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+
+    public virtual ICollection<ClassEnrollment> ClassEnrollments { get; set; } = new List<ClassEnrollment>();
+
+    public virtual ICollection<Grade> Grades { get; set; } = new List<Grade>();
+
+    public virtual Role Role { get; set; } = null!;
+
+    public virtual ICollection<TeacherPerCourse> TeacherPerCourses { get; set; } = new List<TeacherPerCourse>();
+}
